5a77f9f4b983b444f04e43f215398a305336d6eb
mamadj0n <mamadjon398@gmail.com>
2026-06-11 21:50:45 +0330

    Refactor app layout with tabs for Home and Chart

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import time
-
+from random import choices
 # -------------------------
 # Page Config
 # -------------------------
@@ -29,91 +29,117 @@ def load_model():
 model = load_model()
 
 # -------------------------
-# Header
+# tabs
 # -------------------------
-
-st.title("🏠 Tehran House Price Predictor")
-
-st.markdown(
-    "Predict house prices using Machine Learning"
-)
-
-
-st.image("Teharn.jpg", use_container_width=True)
-# -------------------------
-# Sidebar
-# -------------------------
-
-st.header("House Features")
-
-area = st.slider(
-    "Area (m²)",
-    min_value=20,
-    max_value=250,
-    value=100
-)
-
-room = st.slider(
-    "Rooms",
-    min_value=1,
-    max_value=10,
-    value=2
-)
-
-parking = st.checkbox("Parking")
-
-warehouse = st.checkbox("Warehouse")
-
-elevator = st.checkbox("Elevator")
-
-address = st.selectbox(
-    "🔍 Neighborhood",
-    sorted(addresses),
-    index=None,
-    placeholder="Search and select a neighborhood..."
-)
-# -------------------------
-# Input DataFrame
-# -------------------------
-
-input_df = pd.DataFrame({
-    "Area": [area],
-    "Room": [room],
-    "Parking": [int(parking)],
-    "Warehouse": [int(warehouse)],
-    "Elevator": [int(elevator)],
-    "Address": [address]
-})
-
-st.subheader("Input Features")
-
-st.dataframe(input_df)
-
+tab1 , tab2 = st.tabs(['Home' , 'Chart'])
 
 
 # -------------------------
-# Prediction
+# Header home page
 # -------------------------
+with tab1 :
+    st.title("🏠 Tehran House Price Predictor")
 
-if st.button("Predict Price"):
-
-    if address is None:
-        st.error("Please select a neighborhood.")
-        st.stop()
-
-    my_bar = st.progress(0)
-
-    for percent_complete in range(100):
-        time.sleep(0.01)
-        my_bar.progress(percent_complete + 1)
-
-    prediction = model.predict(input_df)
-
-    price = prediction[0]
-
-    st.success("Prediction completed successfully!")
-
-    st.metric(
-        label="Estimated House Price",
-        value=f"{price*3:,.0f} Toman"
+    st.markdown(
+        "Predict house prices using Machine Learning"
     )
+
+
+    st.image("/home/modso/Pictures/Teharn.jpg", use_container_width=True)
+    # -------------------------
+    # Sidebar
+    # -------------------------
+
+    st.header("House Features")
+
+    area = st.slider(
+        "Area (m²)",
+        min_value=20,
+        max_value=250,
+        value=100
+    )
+
+    room = st.slider(
+        "Rooms",
+        min_value=1,
+        max_value=10,
+        value=2
+    )
+
+    parking = st.checkbox("Parking")
+
+    warehouse = st.checkbox("Warehouse")
+
+    elevator = st.checkbox("Elevator")
+
+    address = st.selectbox(
+        "🔍 Neighborhood",
+        sorted(addresses),
+        index=None,
+        placeholder="Search and select a neighborhood..."
+    )
+    # -------------------------
+    # Input DataFrame
+    # -------------------------
+
+    input_df = pd.DataFrame({
+        "Area": [area],
+        "Room": [room],
+        "Parking": [int(parking)],
+        "Warehouse": [int(warehouse)],
+        "Elevator": [int(elevator)],
+        "Address": [address]
+    })
+
+    st.subheader("Input Features")
+
+    st.dataframe(input_df)
+
+    # -------------------------
+    # Prediction
+    # -------------------------
+
+    if st.button("Predict Price"):
+
+        if address is None:
+            st.error("Please select a neighborhood.")
+            st.stop()
+
+        my_bar = st.progress(0)
+
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1)
+
+        prediction = model.predict(input_df)
+
+        price = prediction[0]
+
+        st.success("Prediction completed successfully!")
+
+        st.metric(
+            label="Estimated House Price",
+            value=f"{price*3:,.0f} Toman"
+        )
+
+
+# -------------------------
+# Header chart page
+# -------------------------
+
+with tab2:
+    lite_address = []
+    for loc in choices(addresses):
+        lite_address.append(loc)
+
+    prices = [120, 80, 60, 40, 100, 150, 130, 90, 70, 200]
+
+    df = pd.DataFrame({
+        "Address": addresses,
+        "Price": prices
+    })
+
+    st.bar_chart(df.set_index("Address"))    
+
+
+
